b06cdb2ec875b685508bde8688a2db8aab6c5f27
hji <gkwldls2003@naver.com>
2024-01-17 17:49:44 +0900

    hji 페이징 처리

--- board/board/src/pages/voc/Voc.js
+++ board/board/src/pages/voc/Voc.js
@@ -6,16 +6,28 @@ import CommonTable from '../../components/table/CommonTable';
 import CommonTableColumn from '../../components/table/CommonTableColumn';
 import CommonTableRow from '../../components/table/CommonTableRow';
 import VocHeader from '../../components/VocHeader';
+import VocFooter from "../../components/VocFooter";
+import {useLocation} from "react-router";
 
 function GetData() {
+    const location = useLocation();
     const [data, setData] = useState({});
-    useEffect(() => {
-        axios.get('http://localhost:8080/api/boards').then((response) => {
-            setData(response.data);
-        })
-    }, []);
+    const [pageInfo, setPageInfo] = useState({});
+    let search = null;
 
-    const item = (Object.values(data).filter(vo => vo.deleteYn === 'N')).map((item) => (
+    if(!location.search) {
+        search = "?page=0&size=10"
+    } else {
+        search = location.search;
+    }
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_BOARD_API}` + "/boards" + search).then((response) => {
+            setData(response.data.boards);
+            setPageInfo(response.data);
+        })
+    }, [search]);
+
+    const item = Object.values(data).map((item) => (
         <CommonTableRow key={item.uid}>
             <CommonTableColumn>{item.uid}</CommonTableColumn>
             <CommonTableColumn>
@@ -28,7 +40,10 @@ function GetData() {
         </CommonTableRow>
     ));
 
+    sessionStorage.setItem("pageInfo", JSON.stringify(pageInfo));
     return item;
+
+
 }
 
 function Voc() {
@@ -39,6 +54,7 @@ function Voc() {
         <CommonTable headersName={['글번호', '제목', '등록일', '작성자']}>
             {item}
         </CommonTable>
+        <VocFooter></VocFooter>
     </>);
 }
 

4e96ec994590428b0c67a2bc405753733c5889e4
crushon9 <catco929@naver.com>
2024-01-30 15:41:07 +0900

    writer condition update

--- board/board/src/pages/scm/ScmView.js
+++ board/board/src/pages/scm/ScmView.js
@@ -19,10 +19,13 @@ function ScmDelete(scmId,paging) {
 
 function GetData(scmId) {
     const [data, setData] = useState({});
+    const [userCk, setUserCk] = useState(true);
+    const location = useLocation();
 
     useEffect(() => {
         axios.get('/boardscm/scm/'+scmId).then((response)=> {
             setData(response.data);
+            setUserCk(JSON.parse(sessionStorage.getItem("loginId")) === response.data.writer ? false : true);
         })
     }, []);
 
@@ -42,6 +45,14 @@ function GetData(scmId) {
                 <label>{ data.insertTime }</label>
             </div>
             <div className="scm-view-row">
+                <label>작성자</label>
+                <div className="writer">
+                    {
+                        data.writer
+                    }
+                </div>
+            </div>
+            <div className="scm-view-row">
                 <label>내용</label>
                 <div className="content">
                     {
@@ -49,19 +60,6 @@ function GetData(scmId) {
                     }
                 </div>
             </div>
-        </div></>)
-
-    return item;
-}
-
-function ScmView() {
-    const {scmId} = useParams();
-    const item = GetData(scmId);
-    const location = useLocation();
-
-    return (<>
-        <div>
-            {item}
         </div>
         <div className="scm-footer">
             <Link to={`/scm`+location.state.paging}>
@@ -70,13 +68,26 @@ function ScmView() {
                 </button>
             </Link>
             <Link to={`/scm/update/${scmId}`} state={{paging:location.state.paging}}>
-                <button className="scm-view-go-list-btn">
+                <button className="scm-view-go-list-btn" disabled={userCk}>
                     수정
                 </button>
             </Link>
-                <button className="scm-view-go-list-btn" onClick={(e)=>{ScmDelete(scmId,location.state.paging, e)}}>
-                    삭제
-                </button>
+            <button className="scm-view-go-list-btn" onClick={(e)=>{ScmDelete(scmId,location.state.paging, e)}} disabled={userCk}>
+                삭제
+            </button>
+        </div>
+    </>)
+
+    return item;
+}
+
+function ScmView() {
+    const {scmId} = useParams();
+    const item = GetData(scmId);
+
+    return (<>
+        <div>
+            {item}
         </div>
    </> );
 }

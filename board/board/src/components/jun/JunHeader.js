4b9db01752a2e516c675d6bc700d954ecce17dc4
허 준성 <oksk476@naver.com>
2024-01-31 16:31:01 +0900

    회원가입 페이지 추가(개발중)

--- board/board/src/components/jun/JunHeader.js
+++ board/board/src/components/jun/JunHeader.js
@@ -14,6 +14,11 @@ const VocHeader = props => {
                         게시글 작성
                     </button>
                 </Link>
+                <Link to={`/jun/create`}>
+                    <button align="right" className="jun-view-go-list-btn">
+                        회원가입
+                    </button>
+                </Link>
             </div>
         </div>
     )

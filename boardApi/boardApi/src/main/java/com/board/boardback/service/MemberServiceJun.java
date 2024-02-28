b441baf17edf93de68a407e52cb83563f68b47a6
허 준성 <oksk476@naver.com>
2024-02-28 17:48:25 +0900

    회원가입시 비밀번호 암호화(bCryptPasswordEncoder)

--- boardApi/boardApi/src/main/java/com/board/boardback/service/MemberServiceJun.java
+++ boardApi/boardApi/src/main/java/com/board/boardback/service/MemberServiceJun.java
@@ -3,6 +3,7 @@ package com.board.boardback.service;
 import com.board.boardback.model.jun.MemberJun;
 import com.board.boardback.repository.jun.MemberRepositoryJun;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,7 +15,11 @@ public class MemberServiceJun {
     @Autowired
     private MemberRepositoryJun memberRepositoryJun;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public MemberJun createMember(@RequestBody MemberJun memberJun) {
+        memberJun.setUser_pw((bCryptPasswordEncoder.encode(memberJun.getUser_pw())));
         return memberRepositoryJun.save(memberJun);
     }
 
